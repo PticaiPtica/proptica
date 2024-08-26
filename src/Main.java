760d28efbe373949ee6ea4c6039442e1a38431bf
PticaiPtica <kotx25@yandex.ru>
2024-08-27 00:45:55 +0500

    initial commit 1sss1q1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-    }
-}
+import javax.swing.*;
 
+public class Main {
+        public static void main(String[] args) {
+            JOptionPane.showMessageDialog(null, "Всем большой привет!");
+        }
+    }
