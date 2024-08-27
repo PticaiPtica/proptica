7d35c1c48389fceaa9d3952edaf36cb9f9fda949
PticaiPtica <kotx25@yandex.ru>
2024-08-27 14:49:50 +0500

    initial commit 1sss1q1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ import javax.swing.*;
 
 public class Main {
         public static void main(String[] args) {
-            JOptionPane.showMessageDialog(null, "Всем большой привет!");
+            JOptionPane.showInternalMessageDialog(null, "Всем большой привет!");
         }
     }
