3fb50a9daa7829763de205d736525d540af6b6c2
PticaiPtica <kotx25@yandex.ru>
2024-08-31 15:46:04 +0500

    Boba napisal

--- src/main.java
+++ src/main.java
@@ -1,17 +1,25 @@
 public class main {
     public static void main(String[] args) {
-        byte countRunCycleForField; //
+
+/*        byte countRunCycleForField; //
         countRunCycleForField = 127; //
-        // short=127;
+        short=127;
         int i = 2_000_000_000;
         int Ii = 2_000_000_000;
-        long L = 3_000_000_000L + 2000;
+
         float myFloat = 3.0F;
         double myDouble = 3.3;
         char myChar = 'a';
         String myString = "Hello Wolrd";
         boolean sss = true;
+        var ssss = 22;*/
+        long L = 3_000_000_000L + 2000;
+
+        System.out.println(L);
+
+
     }
+
 }
 
 
