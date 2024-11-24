0707ba5c17be54dad39bc94b491a6a6b77852fe3
PticaiPtica <kotx25@yandex.ru>
2024-11-24 14:09:08 +0500

    11

---
+++ src/GC/GCexample.java
@@ -0,0 +1,21 @@
+package GC;
+
+public class GCexample {
+    public static void main(String[] args) throws InterruptedException {
+        GarbageDemo[] garbageDemos = new GarbageDemo[20];
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                garbageDemos[i] = new GarbageDemo(i);
+            }
+            new GarbageDemo(i);
+
+        }
+        System.out.println("Объекты созданы");
+
+        System.gc();
+
+        Thread.sleep(1000);
+        System.out.println("Завершение");
+        System.out.println(garbageDemos);
+    }
+}
